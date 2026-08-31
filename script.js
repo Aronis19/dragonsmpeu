8719c1a975037a29ef4ffef630213e23b0de2656
Ar0n1s_ <prchlikovazdenka65@gmail.com>
2026-08-31 11:23:48 +0000

    Fix team members

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const asset = {
 const teamGroups = [
   {
     title: "Vedení serveru",
-    members: [["KarelHabsbursky", "Majitel"], ["Kacatkobenzii", "Spolu - Majitel"]],
+    members: [["KarelHabsbursky", "Majitel"], ["Kacatkobenzii", "Spolu - Majitel"], ["Ar0n1sMain_", "Technical Manager"], ["K1ttenMain_", "Hl. Administrátor"]],
   },
   {
     title: "Vedení týmu",
